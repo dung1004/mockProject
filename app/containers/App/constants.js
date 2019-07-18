abc53b9aed61efbfdaff44ed1dc3adff482d51b5
thuan <thuanduy73h1@gmail.com>
2019-07-18 10:12:18 +0700

    tao action goi data

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -8,3 +8,5 @@
  * Follow this format:
  * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
  */
+
+// export const FETCH_DATA = 'containers/App/FETCH_DATA';
---
+++ app/utils/apiCaller.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/named */
+// eslint-disable-next-line import/no-unresolved
+import axios from 'axios';
+import { API_URL } from './constants';
+
+export default function callApi(enpoint, method = 'get', body) {
+  return axios({
+    method,
+    url: `${API_URL}${enpoint}`,
+    data: body,
+  }).catch(err => {
+    console.log(err);
+  });
+}
