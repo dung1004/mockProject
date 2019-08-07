a7c9633dbf7201b3b7392eb6f8a54daa6ed3224f
nguyenduythuan <thuanduy73h1@gmail.com>
2019-08-07 11:04:15 +0700

    update

--- app/containers/Class/saga.js
+++ app/containers/Class/saga.js
@@ -11,18 +11,29 @@ export function* getDataUser() {
   //   const students = yield callApi('students', 'get', null).then(res => [
   //     ...res.data,
   //   ]);
-  const classes = yield callApi('class', 'get', null).then(res => [
-    ...res.data,
-  ]);
+  try {
+    const classes = yield callApi('class', 'get', null).then(res => [
+      ...res.data,
+    ]);
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token) {
+      switch (token.level) {
+        case 0:
+          yield put(fetchClassSuccess(classes));
+          break;
+        case 1:
+          break;
+        case 2:
+          break;
+        default:
+          break;
+      }
+    }
+  } catch (error) {
+    return error;
+  }
+  return null;
   // const staffs = yield callApi('staff', 'get', null).then(res => [...res.data]);
-
-  //   const allData = {
-  //     teacher,
-  //     students,
-  //     classes,
-  //     staffs,
-  //   };
-  yield put(fetchClassSuccess(classes));
 }
 
 export default function* sagaWatcher() {
