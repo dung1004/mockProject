2daf0223fc4a293b390dc2abe0d843b95449a9b9
nguyenduythuan <thuanduy73h1@gmail.com>
2019-08-21 21:56:21 +0700

    xong phan infoUser/index.js

--- app/containers/InfoUser/index.js
+++ app/containers/InfoUser/index.js
@@ -1,170 +1,178 @@
-// import React, { useEffect, memo } from 'react';
-// import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-// import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-// import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-// import Typography from '@material-ui/core/Typography';
-// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// import { connect } from 'react-redux';
-// import { createStructuredSelector } from 'reselect';
-// import PropsTypes from 'prop-types';
-// import { ToastContainer } from 'react-toastify';
+import React, { useEffect, memo } from 'react';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import PropsTypes from 'prop-types';
+import { ToastContainer } from 'react-toastify';
 
-// import { compose } from 'redux';
-// import { useInjectReducer } from 'utils/injectReducer';
-// import { useInjectSaga } from 'utils/injectSaga';
-// import Section from '../../components/Section';
-// import { getData } from './actions';
-// import reducer from './reducers';
-// import { makeSelectData } from './selectors';
-// import { makeSelectLocation } from '../App/selectors';
-// import saga from './sagas';
-// import ItemInfo from '../ItemInfo';
+import { compose } from 'redux';
+import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
+import Section from '../../components/Section';
+import { getData } from './actions';
+import reducer from './reducers';
+import { makeSelectData } from './selectors';
+import { makeSelectLocation } from '../App/selectors';
+import saga from './sagas';
+import ItemInfo from '../ItemInfo';
 
-// const key = 'info';
-// const token = JSON.parse(localStorage.getItem('token'));
-// const admin = 0;
-// const teacher = 1;
-// const student = 2;
+const key = 'info';
 
-// function Info(props) {
-//   useInjectReducer({ key, reducer });
-//   useInjectSaga({ key, saga });
+function Info(props) {
+  useInjectReducer({ key, reducer });
+  useInjectSaga({ key, saga });
+  const token = JSON.parse(localStorage.getItem('token'));
+  const roleAdmin = 0;
+  const roleTeacher = 1;
+  const roleStudent = 2;
 
-//   const id = props.path ? props.path.pathname.slice(12) : null;
+  const [state, setState] = React.useState({
+    teachers: [],
+    students: [],
+  });
 
-//   function checkRole(role, allClass, teachers, students) {
-//     const dataClass = [];
-//     const dataStudent = [];
-//     const dataTeacher = [];
-//     switch (role) {
-//       case admin:
-//         // const dataClass = allClass.filter(cla => cla.id === id);
-//         // allClass.forEach(cla => (cla.id === id ? dataClass.push(cla) : null));
-//         console.log(dataClass[0]);
-//         console.log(role, allClass, teachers, students, 'bcabcb');
-//         dataClass[0].teacherId.forEach(tId => {
-//           console.log("cmm");
-//           teachers.filter(item =>
-//             item.id === tId ? dataTeacher.push(item) : null,
-//           );
-//         });
-//         console.log(dataTeacher);
-//         // students.forEach(item => {
-//         //   item.classId.filter(classId =>
-//         //     classId === dataClass.id ? dataStudent.push(item) : null,
-//         //   );
-//         // });
-//         // console.log(dataClass, dataStudent, dataTeacher);
-//         break;
-//       case teacher:
-//         break;
-//       case student:
-//         break;
+  const id = props.path ? props.path.pathname.slice(12) : null;
 
-//       default:
-//         break;
-//     }
-//   }
+  function getTeacher(arrClass, arrTeacher) {
+    const dataTeacher = [];
+    arrClass[0].teacherId.forEach(tId =>
+      dataTeacher.push(...arrTeacher.filter(item => item.id === tId)),
+    );
+    return dataTeacher;
+  }
 
-//   // const { dataClass, dataStudent, dataTeacher } = props;
-//   // const zxc = props.dataClass
-//   //   ? checkRole(
-//   //       token.level,
-//   //       props.dataClass,
-//   //       props.dataStudent,
-//   //       props.dataTeacher,
-//   //     )
-//   //   : null;
+  function getClass(arr) {
+    const data = arr.filter(cla => cla.id === id);
+    return data;
+  }
 
-//   useEffect(() => {
-//     props.onGetData();
-//   }, []);
+  function getStudent(arr, idClass) {
+    const dataStudent = [];
+    arr.forEach(item =>
+      item.classId.filter(classId =>
+        classId === idClass ? dataStudent.push(item) : null,
+      ),
+    );
+    return dataStudent;
+  }
 
-//   useEffect(() => {
-//     if (props.data && props.data.dataClass) {
-//       console.log(props.data.dataClass);
-//       checkRole(
-//         token.level,
-//         props.data.dataClass,
-//         props.data.dataStudent,
-//         props.data.dataTeacher,
-//       );
-//     }
-//   }, [props.data]);
+  function checkRole(role, allClass, teachers, students) {
+    const updateState = {
+      ...state,
+    };
+    switch (role) {
+      case roleAdmin:
+        {
+          const dataClass = getClass(allClass);
+          updateState.teachers = getTeacher(dataClass, teachers);
+          updateState.students = getStudent(students, dataClass[0].id);
+        }
+        break;
+      case roleTeacher:
+        updateState.students = getStudent(students, id);
+        break;
+      case roleStudent:
+        {
+          const dataClass = getClass(allClass);
+          updateState.teachers = getTeacher(dataClass, teachers);
+        }
+        break;
+      default:
+        break;
+    }
+    setState(updateState);
+  }
 
-//   // console.log(dataClass, dataStudent, dataTeacher);
+  useEffect(() => {
+    props.onGetData();
+  }, []);
 
-//   return (
-//     <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-//       {/* {props.teachers && props.teachers.length > 0 ? (
-//         <Section style={{ width: '50%', padding: '50px 5px' }}>
-//           <h2 style={{ textAlign: 'center' }}>Giáo Viên Đứng Lớp</h2>
-//           {props.teachers.map(teacher => (
-//             <ExpansionPanel key={teacher.id}>
-//               <ExpansionPanelSummary
-//                 expandIcon={<ExpandMoreIcon />}
-//                 aria-controls="panel4bh-content"
-//                 id="panel4bh-header"
-//               >
-//                 <Typography>{teacher.id}</Typography>
-//                 <Typography style={{ marginLeft: '20px', fontWeight: '900' }}>
-//                   {`${teacher.firstName}${teacher.lastName}`}
-//                 </Typography>
-//               </ExpansionPanelSummary>
-//               <ExpansionPanelDetails>
-//                 <ItemInfo teacher={teacher} />
-//               </ExpansionPanelDetails>
-//             </ExpansionPanel>
-//           ))}
-//         </Section>
-//       ) : null}
-//       {props.students && props.students.length > 0 ? (
-//         <Section style={{ width: '50%', padding: '50px 5px' }}>
-//           <h2 style={{ textAlign: 'center' }}>Danh Sách Học Viên</h2>
-//           {props.students.map(student => (
-//             <ExpansionPanel key={student.id}>
-//               <ExpansionPanelSummary
-//                 expandIcon={<ExpandMoreIcon />}
-//                 aria-controls="panel4bh-content"
-//                 id="panel4bh-header"
-//               >
-//                 <Typography>{student.id}</Typography>
-//                 <Typography style={{ marginLeft: '20px', fontWeight: '900' }}>
-//                   {`${student.firstName}${student.lastName}`}
-//                 </Typography>
-//               </ExpansionPanelSummary>
-//               <ExpansionPanelDetails>
-//                 <ItemInfo student={student} />
-//               </ExpansionPanelDetails>
-//             </ExpansionPanel>
-//           ))}
-//         </Section>
-//       ) : null} */}
-//       <ToastContainer autoClose={2000} />
-//     </div>
-//   );
-// }
+  useEffect(() => {
+    if (props.data && props.data.dataClass) {
+      checkRole(
+        token.level,
+        props.data.dataClass,
+        props.data.dataTeacher,
+        props.data.dataStudent,
+      );
+    }
+  }, [props.data]);
 
-// const mapStateToProps = createStructuredSelector({
-//   data: makeSelectData(),
-//   path: makeSelectLocation(),
-// });
-// const mapDispatchToProps = dispatch => ({
-//   onGetData: () => {
-//     dispatch(getData());
-//   },
-// });
-// const withConnect = connect(
-//   mapStateToProps,
-//   mapDispatchToProps,
-// );
-// Info.propTypes = {
-//   onGetData: PropsTypes.func,
-//   data: PropsTypes.object,
-//   path: PropsTypes.object,
-// };
+  return (
+    <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+      {state.teachers.length > 0 ? (
+        <Section style={{ width: '50%', padding: '50px 5px' }}>
+          <h2 style={{ textAlign: 'center' }}>Giáo Viên Đứng Lớp</h2>
+          {state.teachers.map(teacher => (
+            <ExpansionPanel key={teacher.id}>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography>{teacher.id}</Typography>
+                <Typography style={{ marginLeft: '20px', fontWeight: '900' }}>
+                  {`${teacher.firstName}${teacher.lastName}`}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <ItemInfo teacher={teacher} />
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          ))}
+        </Section>
+      ) : null}
+      {state.students.length > 0 ? (
+        <Section style={{ width: '50%', padding: '50px 5px' }}>
+          <h2 style={{ textAlign: 'center' }}>Danh Sách Học Viên</h2>
+          {state.students.map(student => (
+            <ExpansionPanel key={student.id}>
+              <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel4bh-content"
+                id="panel4bh-header"
+              >
+                <Typography>{student.id}</Typography>
+                <Typography style={{ marginLeft: '20px', fontWeight: '900' }}>
+                  {`${student.firstName}${student.lastName}`}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <ItemInfo student={student} />
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          ))}
+        </Section>
+      ) : null}
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+}
 
-// export default compose(
-//   withConnect,
-//   memo,
-// )(Info);
+const mapStateToProps = createStructuredSelector({
+  data: makeSelectData(),
+  path: makeSelectLocation(),
+});
+const mapDispatchToProps = dispatch => ({
+  onGetData: () => {
+    dispatch(getData());
+  },
+});
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+Info.propTypes = {
+  onGetData: PropsTypes.func,
+  data: PropsTypes.object,
+  path: PropsTypes.object,
+};
+
+export default compose(
+  withConnect,
+  memo,
+)(Info);
--- app/containers/InfoUser/sagas.js
+++ app/containers/InfoUser/sagas.js
@@ -21,52 +21,7 @@ export function* getDataInfo() {
       dataStudent: students,
       dataTeacher: teachers,
     };
-    console.log(data);
     yield put(getDataSuccess(data));
-
-    // const token = JSON.parse(localStorage.getItem('token'));
-    // const path = yield select(makeSelectLocation());
-    // const id = yield path.pathname.slice(12);
-    // const getTeacher = [];
-    // const getClass = [];
-    // const getStudent = [];
-    // if (token) {
-    //   switch (token.level) {
-    //     case 0:
-    //       classes.forEach(cla => (cla.id === id ? getClass.push(cla) : null));
-    //       getClass[0].teacherId.forEach(tId => {
-    //         teachers.filter(teacher =>
-    //           teacher.id === tId ? getTeacher.push(teacher) : null,
-    //         );
-    //       });
-    //       students.forEach(student => {
-    //         student.classId.filter(classId =>
-    //           classId === getClass[0].id ? getStudent.push(student) : null,
-    //         );
-    //       });
-    //       yield put(getDataSuccess(getStudent, getTeacher));
-    //       break;
-    //     case 1:
-    //       students.forEach(student => {
-    //         student.classId.filter(classId =>
-    //           classId === id ? getStudent.push(student) : null,
-    //         );
-    //       });
-    //       yield put(getDataSuccess(getStudent, getTeacher));
-    //       break;
-    //     case 2:
-    //       classes.forEach(cla => (cla.id === id ? getClass.push(cla) : null));
-    //       getClass[0].teacherId.forEach(tId => {
-    //         teachers.filter(teacher =>
-    //           teacher.id === tId ? getTeacher.push(teacher) : null,
-    //         );
-    //       });
-    //       yield put(getDataSuccess(getStudent, getTeacher));
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // }
   } catch (error) {
     yield put(getDataFailed(error));
   }
